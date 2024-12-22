f2ed01594f37d6a04413976aacdae808b7845ecf
Gustavo Quintero <gustavoquintero2000@outlook.com>
2024-12-22 10:00:35 -0600

    Ajuste de funcion menuLateral para que ahora tome el menu de la base de datos, tomando los permisos del usuario logueado

--- controladores/globales/php/funciones.php
+++ controladores/globales/php/funciones.php
@@ -101,10 +101,65 @@ function headHtml($modulo, $submodulo)
     print $html;
 }
 
-function menuLateral()
-{
-    // Acceder a la variable global $database
+function menuLateral() {
     global $database;
+
+    // Obtener los módulos disponibles para el usuario (con sus permisos)
+    $resultado = $database->select(
+        "modulos", // Tabla de módulos
+        [
+            "[>]usuarios_modulos" => ["id" => "idmodulo"] // Unir con permisos del usuario
+        ],
+        [
+            "modulos.id", 
+            "modulos.nombre", 
+            "modulos.ruta", 
+            "modulos.icono", 
+            "modulos.padre_id", 
+            "usuarios_modulos.ver"
+        ],
+        [
+            "usuarios_modulos.idusuario" => $_SESSION['idusuario'],
+            "usuarios_modulos.ver" => 1 // Solo mostrar los módulos que el usuario puede ver
+        ]
+    );
+
+    // Agrupar los módulos por su `padre_id`
+    $modulos = [];
+    foreach ($resultado as $modulo) {
+        $modulos[$modulo['padre_id']][] = $modulo;
+    }
+
+    // Función recursiva para generar el menú
+    function generarMenu($padre_id, $modulos) {
+        if (!isset($modulos[$padre_id])) return '';
+
+        $html = '';
+        foreach ($modulos[$padre_id] as $modulo) {
+            // Verificar si tiene submódulos (hijos)
+            $hasChildren = isset($modulos[$modulo['id']]);
+            
+            // Si tiene hijos, es un item "select" (ul con li)
+            if ($hasChildren) {
+                $html .= "<li class=\"nav-item has-treeview\">";
+                $html .= "<a href=\"#\" class=\"nav-link\">";
+                $html .= "<i class=\"nav-icon bi {$modulo['icono']}\"></i>";
+                $html .= "<p>{$modulo['nombre']}<i class=\"nav-arrow bi bi-chevron-right\"></i></p>";
+                $html .= "</a>";
+                $html .= "<ul class=\"nav nav-treeview\">" . generarMenu($modulo['id'], $modulos) . "</ul>";
+                $html .= "</li>";
+            } else {
+                // Si no tiene hijos, es un enlace directo
+                $html .= "<li class=\"nav-item\">";
+                $html .= "<a href=\"{$modulo['ruta']}\" class=\"nav-link\">";
+                $html .= "<i class=\"nav-icon bi {$modulo['icono']}\"></i>";
+                $html .= "<p>{$modulo['nombre']}</p>";
+                $html .= "</a>";
+                $html .= "</li>";
+            }
+        }
+        return $html;
+    }
 
     // Consulta simple para obtener el nombre comercial
     $resultado = $database->select(
@@ -122,43 +177,35 @@ function menuLateral()
     // Si no hay resultados, usar un valor por defecto
     $nombreComercial = $resultado[0] ?? "Mi Empresa";
 
-    // Generar el HTML del menú lateral
+    // HTML del menú lateral
     $html = "
-    <div class=\"app-wrapper\"> <!--begin::Sidebar-->
-        <aside class=\"app-sidebar bg-body shadow\" data-bs-theme=\"dark\"> <!--begin::Sidebar Brand-->
-            <div class=\"sidebar-brand\"> <!--begin::Brand Link--> <a href=\"../index.html\" class=\"brand-link\"> <!--begin::Brand Image--> <img src=\"../../../dist/assets/img/AdminLTELogo.png\" alt=\"AdminLTE Logo\" class=\"brand-image opacity-75 shadow\"> <!--end::Brand Image--> <!--begin::Brand Text--> <span class=\"brand-text fw-light\">$nombreComercial</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
+    <div class=\"app-wrapper\">
+        <aside class=\"app-sidebar bg-body shadow\" data-bs-theme=\"dark\">
+            <div class=\"sidebar-brand\">
+                <a href=\"../index.html\" class=\"brand-link\">
+                    <img src=\"../../../dist/assets/img/AdminLTELogo.png\" alt=\"AdminLTE Logo\" class=\"brand-image opacity-75 shadow\">
+                    <span class=\"brand-text fw-light\">$nombreComercial</span>
+                </a>
+            </div>
             <div class=\"sidebar-wrapper\">
-                <nav class=\"mt-2\"> <!--begin::Sidebar Menu-->
+                <nav class=\"mt-2\">
                     <ul class=\"nav sidebar-menu flex-column\" data-lte-toggle=\"treeview\" role=\"menu\" data-accordion=\"false\">
-                        <li class=\"nav-header\">EXAMPLES</li>
-                        <li class=\"nav-item\"> <a href=\"#\" class=\"nav-link\"> <i class=\"nav-icon bi bi-speedometer\"></i>
-                                <p>
-                                    Dashboard
-                                    <i class=\"nav-arrow bi bi-chevron-right\"></i>
-                                </p>
-                            </a>
-                            <ul class=\"nav nav-treeview\">
-                                <li class=\"nav-item\"> <a href=\"../index.html\" class=\"nav-link\"> <i class=\"nav-icon bi bi-circle\"></i>
-                                        <p>Dashboard v1</p>
-                                    </a> </li>
-                                <li class=\"nav-item\"> <a href=\"../index2.html\" class=\"nav-link\"> <i class=\"nav-icon bi bi-circle\"></i>
-                                        <p>Dashboard v2</p>
-                                    </a> </li>
-                                <li class=\"nav-item\"> <a href=\"../index3.html\" class=\"nav-link\"> <i class=\"nav-icon bi bi-circle\"></i>
-                                        <p>Dashboard v3</p>
-                                    </a> </li>
-                            </ul>
-                        </li>
-                    </ul> <!--end::Sidebar Menu-->
+                        <li class=\"nav-header\">EXAMPLES</li>";
+
+    // Generar el menú dinámico
+    $html .= generarMenu(null, $modulos);
+
+    $html .= "
+                    </ul>
                 </nav>
-            </div> <!--end::Sidebar Wrapper-->
-        </aside> <!--end::Sidebar-->
-    </div>
-    ";
+            </div>
+        </aside>
+    </div>";
 
     // Imprimir el HTML
     print $html;
 }
+
 
 
 
