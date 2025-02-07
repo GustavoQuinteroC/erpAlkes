77562a6058bc355ea94349af69fbf07be4f33447
Gustavo Quintero <gustavoquintero2000@outlook.com>
2025-02-06 21:25:25 -0600

    creacion de funcionmes encargadas de habilitar y deshabilitar los campos llaves para el documento, en este caso almacen y concepto

--- controladores/individuales/almacén.movimientos.php
+++ controladores/individuales/almacén.movimientos.php
@@ -63,6 +63,72 @@ class almacenMovimientos extends alkesGlobal
         return $this->response;
     }
 
+    function deshabilitaAlmacen()
+    {
+        $this->deshabilitaSelect('idalmacen');
+        $this->response->assign("cambiarAlmacen", "disabled", "");
+        return $this->response;
+    }
+
+    function deshabilitaConcepto()
+    {
+        $this->response->assign("cambiarConcepto", "disabled", "");
+        $this->deshabilitaSelect('idconcepto');
+        return $this->response;
+    }
+
+    function modalConfirmacionHabilitaAlmacen()
+    {
+        $this->alertaConfirmacion("¡CUIDADO!", "Si habilita el cambio de almacén se borraran todas las partidas ya agregadas ¿Desea continuar?", "warning", "JaxonalmacenMovimientos.habilitaAlmacen();");
+        return $this->response;
+    }
+
+    function modalConfirmacionHabilitaConcepto()
+    {
+        $this->alertaConfirmacion("¡CUIDADO!", "Si habilita el cambio de concepto se borraran todas las partidas ya agregadas ¿Desea continuar?", "warning", "JaxonalmacenMovimientos.habilitaConcepto();");
+        return $this->response;
+    }
+
+    function habilitaAlmacen()
+    {
+        $this->habilitaSelect('idalmacen');
+        $this->response->script('
+            // Desactivar temporalmente el evento onchange para los selects que tienen onchange y necesiten un change (select2)
+            document.getElementById("idalmacen").onchange = null;
+
+            // Asignar valores a los campos select2 sin disparar onchange
+            $("#idalmacen").val("").trigger("change.select2");
+
+            // Restaurar el evento onchange original para los selects que tienen onchange y necesiten un change (select2)
+            document.getElementById("idalmacen").onchange = function() {
+                JaxonalmacenMovimientos.deshabilitaAlmacen();
+            };
+        ');
+        $this->response->assign("cambiarAlmacen", "disabled", "disabled");
+        $this->desactivarTodasLasPartidas();
+        return $this->response;
+    }
+
+    function habilitaConcepto()
+    {
+        $this->habilitaSelect('idconcepto');
+        $this->response->script('
+            // Desactivar temporalmente el evento onchange para los selects que tienen onchange y necesiten un change (select2)
+            document.getElementById("idconcepto").onchange = null;
+
+            // Asignar valores a los campos select2 sin disparar onchange
+            $("#idconcepto").val("").trigger("change.select2");
+
+            // Restaurar el evento onchange original para los selects que tienen onchange y necesiten un change (select2)
+            document.getElementById("idconcepto").onchange = function() {
+                JaxonalmacenMovimientos.deshabilitaAlmacen();
+            };
+        ');
+        $this->response->assign("cambiarConcepto", "disabled", "disabled");
+        $this->desactivarTodasLasPartidas();
+        return $this->response;
+    }
+
 }
 
 
